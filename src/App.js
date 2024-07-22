637cc8a61476dc4df83163e596c453142a8ec65e
Mehul <mehkumawat@gmail.com>
2024-07-23 00:53:31 +0530

    starting the app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800 text-center App">
+        Netflix GPT
+    </div>
+  );
+}
+
+export default App;
